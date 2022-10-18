162e547eaa5651875abd4ec9ff8d7ae14d977186
PsychoBaddy <asifhassan1996@gmail.com>
2022-10-18 17:45:51 +0530

    first commit

---
+++ JavaPRACTICE/src/methodhiding/SuperClass.java
@@ -0,0 +1,20 @@
+package methodhiding;
+
+public class SuperClass {
+	
+	public static void staticMethod() {
+		System.out.println("I am staticMethod in SuperClass ");
+		}
+	  public void instanceMethod() {
+		  System.out.println("I am instanceMethod in SuperClass ");
+		  }
+
+	public static void main(String[] args) {
+		
+		
+		
+		
+
+	}
+
+}
